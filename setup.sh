641840bcafc1e42284f588cfce8f028df4a2f4ed
adidude <menon.or.adithya@gmail.com>
2021-04-01 14:01:13 +0300

    Targeted setup.sh for arch and made the setup generic for any user.

--- setup.sh
+++ setup.sh
@@ -1,35 +1,58 @@
-#dependencies and powertop
-sudo apt -y install snapd gdb git cmake powertop
-#visual studio code
-sudo snap install code --classic
+#dependencies
+#sudo apt -y install gdb &
+#sudo apt -y install git &
+#sudo apt -y install cmake &
+echo "~gdb~"
+sudo pacman --noconfirm -S gdb
+echo "~gcc~"
+sudo pacman --noconfirm -S gcc
+echo "~cmake~"
+sudo pacman --noconfirm -S cmake
+echo "~make~"
+sudo pacman --noconfirm -S make
+
 #pip
-sudo apt install -y python-pip
+#sudo apt install -y python-pip &
+#sudo apt -y install snapd
+echo "~pip~"
+sudo pacman --noconfirm -S python-pip
+#echo "~snapd~"
+#sudo pacman --noconfirm -S snapd
+wait
+#visual studio code
+echo "~visual studio code~"
+#sudo snap install code --classic &
+sudo pacman --noconfirm -S code
 #s-tui
-sudo apt install s-tui
+echo "~s-tui~"
+sudo pip install s-tui &
 
 #Fastflow
-if [ ! -d "/home/adi/Utility" ]; then
-    sudo mkdir /home/adi/Utility
-    sudo mkdir /home/adi/Utility/fastflow
-    git clone https://github.com/fastflow/fastflow.git /home/adi/Utility/fastflow
-elif [ ! -d "/home/adi/Utility/fastflow" ]; then 
-    sudo mkdir /home/adi/Utility/fastflow
-    git clone https://github.com/fastflow/fastflow.git /home/adi/Utility/fastflow
+echo "~FastFlow~"
+if [ ! -d "/home/$USER/Utility" ]; then
+    sudo mkdir /home/$USER/Utility
+    sudo mkdir /home/$USER/Utility/fastflow
+    sudo git clone https://github.com/fastflow/fastflow.git /home/$USER/Utility/fastflow
+elif [ ! -d "/home/$USER/Utility/fastflow" ]; then 
+    sudo mkdir /home/$USER/Utility/fastflow
+    sudo git clone https://github.com/fastflow/fastflow.git /home/$USER/Utility/fastflow
 fi
 
-export FF_ROOT=/home/adi/Utility/fastflow
-sudo echo "export FF_ROOT=/home/adi/Utility/fastflow" >> ~/.bashrc
+export FF_ROOT=/home/$USER/Utility/fastflow
+sudo echo "export FF_ROOT=/home/$USER/Utility/fastflow" >> ~/.bashrc
 
-if [ ! -d "/home/adi/Utility/fastflow/build" ]; then
-    sudo mkdir /home/adi/Utility/fastflow/build
+if [ ! -d "/home/$USER/Utility/fastflow/build" ]; then
+    sudo mkdir /home/$USER/Utility/fastflow/build
 fi
 cd $FF_ROOT/build
-cmake /home/adi/Utility/fastflow
-make
-make test
+sudo cmake /home/$USER/Utility/fastflow
+sudo make
+sudo make test
 sudo make install DESTDIR=/
-sudo cp -r /home/adi/Utility/fastflow/ff /usr/local/include
+sudo cp -r /home/$USER/Utility/fastflow/ff /usr/local/include
 
+#Wait for background jobs to finish
+wait
 #~~~~~~IntelTBB - now Intel OneAPI Thread Building Blocks ~WIP~~~~~~
 #sudo apt-get install -y gpg-agent
 #wget -qO - https://repositories.intel.com/graphics/intel-graphics.key | sudo apt-key add -
