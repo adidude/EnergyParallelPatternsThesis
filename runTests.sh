d706004611e474cde58669b36d16535575892629
adidude <menon.or.adithya@gmail.com>
2021-04-26 00:29:20 +0300

    Added stitcher script to merge energy readings and speedup readings.

--- runTests.sh
+++ runTests.sh
@@ -5,4 +5,3 @@ sudo kill -9 $PROC
 sudo kill $(ps aux | grep '[.]/batReader.sh' | awk '{print $2}')
 sudo rm nohup.out
 python ./PyScripts/stitcher.py
-curl -X POST https://maker.ifttt.com/trigger/task_complete/with/key/eBm552XQq4pOPLH5ncoBSwDKECuQjM1EujlATzOOUM2
